f9624106b62f8db30e20317fb924e4c9b6d86579
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-06-28 08:21:52 -0700

    Add ACS_CONNECTION_STRING validation and configure forwarded headers in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -4,3 +4,19 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration["ACS_CONNECTION_STRING"];
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+  throw new InvalidOperationException("ACS_CONNECTION_STRING is required. Run 'make provision' first.");
+}
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+  options.ForwardedHeaders = ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto;
+});
+builder.Services.AddSingleton(new CallAutomationClient(connectionString));
+
+var app = builder.Build();
+
+app.UseForwardedHeaders();
+
